b933a456cf7d45b22b605d7c9520ae01e5a5e8d0
wwwyyuu <wangyu0917@hotmail.com>
2015-11-27 12:16:24 +0800

    Update hm_mgp_dsb.py
    added in grouping function
    Grouping rules	One-piece		One-piece
    	           Jumpsuits/playsuit	One-piece
    	     Blazers&Kimonos		Outerwear
    	                  Bottoms		Bottoms
                           dress/dresses     Dress

--- hm_mgp_dsb.py
+++ hm_mgp_dsb.py
@@ -37,43 +37,111 @@ shop text)""");
 conn.commit()
 query = """insert or ignore into Price_data_2015_11_20_01 values(?,?,?,?)"""
 
-
-
-#dsb get product catgory list
-dsb_dress = myopener.open("http://www.dressabelle.com.sg/").read()
-soup_dsb = BeautifulSoup(dsb_dress,"html.parser")
-dsb_category_list = {}
-for category in soup_dsb.find_all("li", class_="inactive parent deeper")[1].find_all("li"):
-    dsb_category_list[category.find("a").getText()] ="http://www.dressabelle.com.sg"+category.find("a")["href"]
-
-
-#mgp get product catgory list
-mgp_html = myopener.open("https://mgplabel.com/").read()
-soup_mgp = BeautifulSoup(mgp_html,"html.parser")
-mgp_category_list = {}
-for category in soup_mgp.find("ul",class_="sf-menu").find_all("li")[2].find_all("li"):
-    mgp_category_list[category.find("a").getText()] =category.find("a")["href"]
-
-
-#h&m get product category list
-hm_dress = myopener.open("http://www.hm.com/sg/subdepartment/LADIES").read()
-soup_hm = BeautifulSoup(hm_dress,"html.parser")
-hm_category_list = {}
-for category in soup_hm.find("ul",class_="products single").find_all("a"):
-    hm_category_list[category.getText()] = category["href"]
-
-
 if __name__ == "__main__":
+    
+    #dsb get product catgory list
+    dsb_dress = myopener.open("http://www.dressabelle.com.sg/").read()
+    soup_dsb = BeautifulSoup(dsb_dress,"html.parser")
+    dsb_category_list = {}
+    for category in soup_dsb.find_all("li", class_="inactive parent deeper")[1].find_all("li"):
+        dsb_category_list[category.find("a").getText()] ="http://www.dressabelle.com.sg"+category.find("a")["href"]
+    
+    #mgp get product catgory list
+    mgp_html = myopener.open("https://mgplabel.com/").read()
+    soup_mgp = BeautifulSoup(mgp_html,"html.parser")
+    mgp_category_list = {}
+    for category in soup_mgp.find("ul",class_="sf-menu").find_all("li")[2].find_all("li"):
+        mgp_category_list[category.find("a").getText()] =category.find("a")["href"]
+    
+    #h&m get product category list
+    hm_dress = myopener.open("http://www.hm.com/sg/subdepartment/LADIES").read()
+    soup_hm = BeautifulSoup(hm_dress,"html.parser")
+    hm_category_list = {}
+    for category in soup_hm.find("ul",class_="products single").find_all("a"):
+        hm_category_list[category.getText()] = category["href"]
+    
+    def HM_scraper(category):
+        print "------------"
+        print "current category:" + category
+        current_page = 1
+        end_page =False
+        counter = 0
+        while not end_page:
+            if current_page == 1:
+                hm_dress = myopener.open(hm_category_list[category]).read()
+                soup_hm = BeautifulSoup(hm_dress,"html.parser")
+            print "current page:" + str(current_page)
+            for item in soup_hm.find_all("span",class_="product-info"):
+                c.execute(query,(item.find("span",class_="details").getText().strip(),item.find("span",class_="price").getText().strip() ,category,"H&M",str(date.today())))
+                counter +=1
+                conn.commit()
+            #print item.find("span",class_="details").getText().strip()
+            #print item.find("span",class_="price").getText().strip()  
+            try:
+                hm_dress = myopener.open("http://www.hm.com/sg/subdepartment/LADIES"+soup_hm.find("li",class_="next").find('a')['href']).read()
+                #print "http://www.hm.com/sg/subdepartment/LADIES"+soup_hm.find("li",class_="next").find('a')['href']
+                soup_hm = BeautifulSoup(hm_dress,"html.parser")
+                current_page += 1
+            except:
+                end_page = True
+        print str(counter) +" "+category+" from H&M are downloaded"
+ 
+    def dsb_scraper(category):
+        print "------------"
+        print "current category:" + category
+        current_page = 1
+        end_page =False
+        counter = 0
+        while not end_page:
+            if current_page == 1:
+                page_html = myopener.open(dsb_category_list[category]).read()
+                soup_dsb = BeautifulSoup(page_html,"html.parser")
+            print "current page:" + str(current_page)
+            for divs in soup_dsb.find_all("div",class_="quick-view-cont hidden-phone hidden-tablet"):
+                c.execute(query,(divs.find("div",class_="title-container").find("h3").find("a").getText(),divs.find("div",class_="PricesalesPrice").find("span", class_="PricesalesPrice").getText(),category,"dsb",str(date.today())))
+                counter += 1
+                conn.commit()
+                #print divs.find("div",class_="title-container").find("h3").find("a").getText()
+            try:
+                page_html = myopener.open("http://www.dressabelle.com.sg"+soup_dsb.find("a",class_="pagenav next")['href']).read()
+                #print "http://www.dressabelle.com.sg"+soup_dsb.find("a",class_="pagenav next")['href']
+                soup_dsb = BeautifulSoup(page_html,"html.parser")
+                current_page += 1
+            except:
+                end_page = True
+        print str(counter) +" "+category+" from dressabelle are downloaded"     
+    
+    #grouping function
+    def grouping(dfs):
+        df = dfs
+        for i in range(len(df['category'])):
+            if "one-piece" in df['category'][i].lower():
+                df['category'][i] = "One-Piece"
+            elif ("jumpsuit" in df["name"][i].lower()) or ("playsuit" in df["name"][i].lower()):
+                df['category'][i] = "One-Piece"                             
+            elif "blazer" in  df['category'][i].lower():
+                df['category'][i] = 'Outerwear'
+            elif 'bridesmaid' in df['category'][i].lower():
+                df['category'][i] = 'Bridesmaid'
+            elif 'dress' in df['category'][i].lower():
+                df['category'][i] = 'Dress'
+            elif "jacket" in df['category'][i].lower():
+                df['category'][i] = 'Outerwear'
+    
+        return df
+    
     #SCRAPE h&m
     print "----------------"
     print "scraping h&m.com"
     for cat in hm_category_list.keys():
         HM_scraper(cat)
+        
     #dsb scrape
     print "----------------"
     print "scraping dressabelle.com"
     for cat in dsb_category_list:
         dsb_scraper(cat)
+        
     #mgblabel    
     print "----------------"
     print "scraping mgpleble.com"
@@ -82,62 +150,16 @@ if __name__ == "__main__":
         mgb_dress = myopener.open(mgp_category_list[category]+"?n=10000").read()
         soup_mgb = BeautifulSoup(mgb_dress,"html.parser")
         for item in soup_mgb.find("ul",id="product_list").find_all("div",class_="right_block"):
-            c.execute(query,(item.find("a").getText(),item.find("span",class_="price").getText(),category,"mgblabel"))
+            c.execute(query,(item.find("a").getText(),item.find("span",class_="price").getText(),category,"mgplabel",str(date.today())))
             counter += 1
-        print str(counter) +" "+category+" from mgblabel are downloaded"
+        print str(counter) +" "+category+" from mgplabel are downloaded"
     conn.commit()
-    print "data collection done!"
-    conn.close()
-
-
-def HM_scraper(category):
-    print "------------"
-    print "current category:" + category
-    current_page = 1
-    end_page =False
-    counter = 0
-    while not end_page:
-        if current_page == 1:
-            hm_dress = myopener.open(hm_category_list[category]).read()
-            soup_hm = BeautifulSoup(hm_dress,"html.parser")
-        print "current page:" + str(current_page)
-        for item in soup_hm.find_all("span",class_="product-info"):
-            c.execute(query,(item.find("span",class_="details").getText().strip(),item.find("span",class_="price").getText().strip() ,category,"H&M"))
-            counter +=1
-            conn.commit()
-            #print item.find("span",class_="details").getText().strip()
-            #print item.find("span",class_="price").getText().strip()  
-        try:
-            hm_dress = myopener.open("http://www.hm.com/sg/subdepartment/LADIES"+soup_hm.find("li",class_="next").find('a')['href']).read()
-            #print "http://www.hm.com/sg/subdepartment/LADIES"+soup_hm.find("li",class_="next").find('a')['href']
-            soup_hm = BeautifulSoup(hm_dress,"html.parser")
-            current_page += 1
-        except:
-            end_page = True
-    print str(counter) +" "+category+" from H&M are downloaded"
- 
-
-def dsb_scraper(category):
-    print "------------"
-    print "current category:" + category
-    current_page = 1
-    end_page =False
-    counter = 0
-    while not end_page:
-        if current_page == 1:
-            page_html = myopener.open(dsb_category_list[category]).read()
-            soup_dsb = BeautifulSoup(page_html,"html.parser")
-        print "current page:" + str(current_page)
-        for divs in soup_dsb.find_all("div",class_="quick-view-cont hidden-phone hidden-tablet"):
-            c.execute(query,(divs.find("div",class_="title-container").find("h3").find("a").getText(),divs.find("div",class_="PricesalesPrice").find("span", class_="PricesalesPrice").getText(),category,"dsb"))
-            counter += 1
-            conn.commit()
-            #print divs.find("div",class_="title-container").find("h3").find("a").getText()
-        try:
-            page_html = myopener.open("http://www.dressabelle.com.sg"+soup_dsb.find("a",class_="pagenav next")['href']).read()
-            #print "http://www.dressabelle.com.sg"+soup_dsb.find("a",class_="pagenav next")['href']
-            soup_dsb = BeautifulSoup(page_html,"html.parser")
-            current_page += 1
-        except:
-            end_page = True
-    print str(counter) +" "+category+" from dressabelle are downloaded"
+    
+    print "data collecton done!"
+    print "-----------"
+    df = pandas.read_sql("select * from Price_data_final where Date = "+"\""+str(date.today())+"\"", conn)
+    df = grouping(df)
+    df.to_excel("Price data on "+str(date.today())+".xlsx")
+    print "-----------"
+    print "Data file: "+"Price_data_on_"+str(date.today())+".xlsx is created at "+str(os.getcwd())
+    conn.close
